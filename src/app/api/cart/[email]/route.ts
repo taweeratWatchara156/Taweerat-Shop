4e2b3bd0fb05f38c51f250cc03e2bf1d51505fdc
GangSom <129844552+taweeratWatchara156@users.noreply.github.com>
2024-11-29 18:14:45 +0700

    Fixed Cart Bug

--- src/app/api/cart/[email]/route.ts
+++ src/app/api/cart/[email]/route.ts
@@ -2,41 +2,25 @@ import { NextRequest, NextResponse } from "next/server";
 import { connectMongoDB } from "../../../../../lib/mondodb";
 import User from "../../../../../models/user";
 
-type Params = {
-    params: {
-        email: string;
-    };
-};
-
-export async function GET(
-    req: NextRequest,
-    { params }: Params
-) {
-    try {
-        await connectMongoDB();
-
+export async function GET(req:NextRequest, { params }: { params: { email:string } }){
+    try{
+        await connectMongoDB()
         const { email } = params;
+        const user = await User.findOne({ email })
 
-        const user = await User.findOne({ email });
-
-        if (!user) {
-            return NextResponse.json({
-                success: false,
-                message: "User not found",
-            });
-        }
+        if(!user) return NextResponse.json({ success:false, message: "User not found" })
 
         const cart = user.cart;
 
         return NextResponse.json({
-            success: true,
+            success:true,
             cart,
-            length: Object.keys(cart || {}).length,
-        });
-    } catch (error: any) {
+            length: Object.keys(Object.fromEntries(cart)).length
+        })
+    }catch(error:any){
         return NextResponse.json({
-            success: false,
-            message: error.message,
-        });
+            success:false,
+            message:error.message
+        })
     }
 }
